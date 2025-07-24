4ed235e0ee32877323e68fe4ddea34815512bb58
Sakthi-viji <152084087+Sakthi-viji@users.noreply.github.com>
2025-07-24 11:01:00 +0530

    Delete fridgecart-backend directory

--- fridgecart-backend/fridge_detector.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-def detect_items(image_file):
-    # This is mock logic
-    image = Image.open(image_file)
-    return ["banana", "apple", "oats"]
--- fridgecart-backend/test_view_cart.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:5000/view-cart"
-
-response = requests.get(url)
-
-print("Status Code:", response.status_code)
-print("Cart Contents:", response.json())
